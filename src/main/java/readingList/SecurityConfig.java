5a378106b91ddef2517acef0b5991d0347a8319f
x75huang <x75huang@uwaterloo.ca>
2016-11-10 23:20:39 +0800

    Configure profile

--- src/main/java/readingList/SecurityConfig.java
+++ src/main/java/readingList/SecurityConfig.java
@@ -2,6 +2,7 @@ package readingList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 /**
  * Created by Lynne on 2016-11-06.
  */
+@Profile("production")
 @Configuration
 @EnableWebSecurity //indirectly creating a bean of type WebSecurityConfiguration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
